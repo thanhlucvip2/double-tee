86541b2b7f9b3654c545d20044274c553c051a91
lucdticd-vn <lucdt@icd-vn.com>
2023-05-19 16:52:23 +0700

    a

--- src/apis/import-products/import-products-detail/entities/import-products-detail.entity.ts
+++ src/apis/import-products/import-products-detail/entities/import-products-detail.entity.ts
@@ -2,4 +2,25 @@ import { BaseEntity } from '@/systems/base.entity';
 import { Column, Entity } from 'typeorm';
 
 @Entity('tb_import_product_detail')
-export class ImportProductsDetail extends BaseEntity {}
+export class ImportProductsDetail extends BaseEntity {
+  @Column({ type: 'varchar', nullable: false })
+  sku: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  size: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  color: string;
+
+  @Column({ type: 'int', nullable: false })
+  price: string;
+
+  @Column({ type: 'int', nullable: false })
+  quantity: string;
+
+  @Column({ type: 'int', nullable: false })
+  total_price: string;
+
+  @Column({ type: 'int', nullable: true, default: 0 })
+  down_price: string;
+}
--- src/constants/import_products_order.ts
+++ src/constants/import_products_order.ts
@@ -1,4 +1,5 @@
 export const IMPORT_PRODUCTS_ORDER = {
-  CREATE: 'CREATE',
-  DONE: 'DONE',
+  CREATE: 'CREATE', // mới tạo
+  PROCESSING: 'PROCESSING', // đang xử lý
+  DONE: 'DONE', // đã nhập
 };
